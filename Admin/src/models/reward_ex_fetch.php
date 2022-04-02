e17dd5c42b8a2fa614509300e0ac63893efb7340
THitiya <45777890+thitiya035@users.noreply.github.com>
2022-04-02 17:52:18 +0700

    fix reward connect

--- Admin/src/models/reward_ex_fetch.php
+++ Admin/src/models/reward_ex_fetch.php
@@ -1,5 +1,5 @@
 <?php
-require '../../Mysql/Connect.php';
+require "../Mysql/Connect.php";
 
 $db_fetch_ex = "SELECT * FROM log_exchange_reward";
 $result_fetch_ex = mysqli_query($dbcon, $db_fetch_ex);
